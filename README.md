71c916409365cfbb408e20c17469e60d20385126
unknown <ghaliarose89@gmail.com>
2021-11-04 16:27:32 -0500

    fixing bug

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
 # Note Taker 
+## Purpose:
+ Note Taker web app can be used to write and save notes.
+
+### Description:
+WHEN opening the Note Taker
+the app presented with a landing page with a link to a notes page
+clicking on the link to the notes page
+THEN I am presented with a page with existing notes listed in the left-hand column, plus empty fields to enter a new note title and the note’s text in the right-hand column
+WHEN I enter a new note title and the note’s text
+THEN a Save icon appears in the navigation at the top of the page
+WHEN I click on the Save icon
+THEN the new note I have entered is saved and appears in the left-hand column with the other existing notes
+WHEN I click on an existing note in the list in the left-hand column
+THEN that note appears in the right-hand column
+WHEN I click on the Write icon in the navigation at the top of the page
+THEN I am presented with empty fields to enter a new note title and the note’s text in the right-hand column
+```
+
+
+
+## instructions : 
+* Install the npm package then enter 'node index' 
+* Questions will prompt and after answering, the index.html file will be created.
+* open the index file from the dir folder and team members will be displayed there.
+
+## Usage:
+![snip](./dist/c2.JPG)
+
+The link below is a walkthrough video for the app
+
+https://youtu.be/B1AI34St_SQ
+
+## Testes:
+All tests are passed.
+
+![snip](./dist/Capture.JPG)
+
+### Contribution:
+Made with ❤️️ by Ghalia Sami. 
+For more information, Please contact me.
+Email: ghaliarose89@gmail.com
+
+### ©️ [2021] [Ghalia Sami]
+
--- server.js
+++ server.js
@@ -2,15 +2,22 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const app = express();
+//dependientes
 const PORT = process.env.PORT || 3001;
+//npm uniq id
+const { v1: uuidv1 } = require('uuid');
+//middleware>>>
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use(express.json());
 
+
+//index route
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/notes.html'));
 });
 
+// note route
 app.get('/api/notes', (req, res) => {
     fs.readFile('db/db.json', 'utf8', (err, data) => {
         if (err) console.log(err)
@@ -20,50 +27,49 @@ app.get('/api/notes', (req, res) => {
         }
     });
 });
-
+// adding notes api
 app.post('/api/notes', (req, res) => {
-    
+
     fs.readFile('db/db.json', 'utf8', (err, data) => {
 
-            const notes = JSON.parse(data);
-            const newNote = {
-                id: notes.length+1,
-                title: req.body.title,
-                text: req.body.text
-            };
-            console.log(`id=${newNote.id}`)
-             notes.push(newNote);
-        const newdata = JSON.stringify(notes,null,4);
-        fs.writeFile ('db/db.json',newdata, err =>{
+        const notes = JSON.parse(data);
+        const newNote = {
+            id: uuidv1(notes.length + 1),
+            title: req.body.title,
+            text: req.body.text
+        };
+
+        notes.push(newNote);
+        const newdata = JSON.stringify(notes, null, 4);
+        fs.writeFile('db/db.json', newdata, err => {
             if (err) throw err;
             res.json(newNote)
         });
-       
+
     });
 
 });
-    app.delete ('/api/notes/:id' ,(req, res) => {
-        fs.readFile('db/db.json', 'utf8', (err, data) => {
-            const notes = JSON.parse(data);
-           const deletedNotes = notes.filter(notes => notes.id!== parseInt(req.params.id));
-           console.log (req.params.id);
-           console.log(`deletedNotes=${deletedNotes}`);
-           const newdata = JSON.stringify(deletedNotes,null,4);
-           console.log(`newdata${newdata}`);
-           fs.writeFile ('db/db.json',newdata, err =>{
-               if (err) throw err;
-               res.json(notes);
-           });
 
-    } );
+//deleting api
+app.delete('/api/notes/:id', (req, res) => {
+    fs.readFile('db/db.json', 'utf8', (err, data) => {
+        const notes = JSON.parse(data);
+        const deletedNotes = notes.filter(notes => notes.id !== req.params.id);
+        const newdata = JSON.stringify(deletedNotes, null, 4);
+        fs.writeFile('db/db.json', newdata, err => {
+            if (err) throw err;
+            res.json(notes);
+        });
+
+    });
 });
 
+// index route
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+});
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'public/index.html'));
-    });
-
-
-    app.listen(PORT, () => {
-        console.log(`API server now on port ${PORT}!`);
-    });
+//listning port
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
